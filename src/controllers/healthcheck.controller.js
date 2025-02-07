1eb923c6ffc5055acd103d5c3b119db9195264a9
Aditya <adityarawat1512@gmail.com>
2025-02-07 21:43:49 +0530

    Add healthcheck controller with uptime and timestamp response

---
+++ src/controllers/healthcheck.controller.js
@@ -0,0 +1,19 @@
+import {ApiResponse} from "../utils/ApiResponse.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
+
+
+const healthcheck = asyncHandler(async (req, res) => {
+    res
+    .status(200)
+    .json(new ApiResponse(200,{
+        status: "OK",
+        uptime: process.uptime(),  // Server uptime in seconds
+        timestamp: new Date().toISOString()  // Current server time
+    },"Everything is ok"))
+
+    //TODO: build a healthcheck response that simply returns the OK status as json with a message
+})
+
+export {
+    healthcheck
+    }
